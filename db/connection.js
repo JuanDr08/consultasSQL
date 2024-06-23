8ef48861b467a9534ff1e974425eefc4f9d47095
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-06-22 22:19:35 -0500

    feat: :sparkles: Added the conexion

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,9 @@
 import mysql from 'mysql2/promise';
+
+export const conexion = await mysql.createConnection({
+    host: 'viaduct.proxy.rlwy.net',
+    port: '30246',
+    user: 'root',
+    password: 'tBvtWOqbjscWtxInVClNjgzcrfKZtlSW',
+    database: 'railway'
+})
